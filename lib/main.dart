feeecd85a8517af18eaa953594575eef4d12b68b
pirate422 <yash332265@gmail.com>
2026-09-23 14:25:19 +0530

    to verify PR sync

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
+// BillBuddy Main Application Entry Point
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'presentation/router/app_router.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: BillBuddyApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: BillBuddyApp()));
 }
 
 class BillBuddyApp extends ConsumerWidget {
